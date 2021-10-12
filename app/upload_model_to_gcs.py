433c4bc11641a1ff21258db859567b7ef1676967
Antoine Vo <antoine@blueberrymed.com>
2021-10-12 10:48:26 -0700

    no message

--- app/upload_model_to_gcs.py
+++ app/upload_model_to_gcs.py
@@ -1,5 +1,12 @@
 import utils.google_cloud_storage as gcs
 
 gcs.upload_model_to_gcs('zoo/teamfighttactics/base.zip')
-# gcs.upload_best_model_to_gcs('zoo/teamfighttactics/tmp/best_model.zip')
+
+
+try:
+    gcs.upload_best_model_to_gcs('zoo/teamfighttactics/tmp/best_model.zip')
+except Exception:
+    print("No best model to upload")
+
+
 gcs.upload_to_bucket('logs', 'tft_models', 'logs')
